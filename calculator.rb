cd34c71a7dd23541bc34f58d5d99d0df2b7e1ed1
qiashika <ashika.jain@quantuminventions.com>
2023-02-03 19:01:08 +0530

    [R - 99392] Switch case inside method oop concept implement

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,26 @@ class Calculator
     def initialize
         @result=0
     end
+    def options choice,num1,num2
+        case choice.to_i
+        when 1
+            p "#{num1} + #{num2} = #{add num1,num2}"
+        when 2
+            p "#{num1} - #{num2} = #{sub num1,num2}"
+        when 3
+            p "#{num1} * #{num2} = #{multiply num1,num2}"
+        when 4
+            begin
+                p "#{num1} / #{num2} = #{division num1,num2}"
+            rescue
+                p "Denominator cannot be 0\n Enter another number "
+                num2=gets.chomp()
+                p "#{num1} / #{num2} = #{division num1,num2}"
+            end
+        else
+            p "Invalid option "
+        end
+    end
     def add num1,num2
         @result= num1.to_i+num2.to_i
     end
@@ -26,21 +46,4 @@ num1=gets.chomp()
 puts "Enter a number"
 num2=gets.chomp()
 
-case choice.to_i
-when 1
-   p "#{num1} + #{num2} = #{c.add num1,num2}"
-when 2
-    p "#{num1} - #{num2} = #{c.sub num1,num2}"
-when 3
-    p "#{num1} * #{num2} = #{c.multiply num1,num2}"
-when 4
-    begin
-        p "#{num1} / #{num2} = #{c.division num1,num2}"
-    rescue
-        p "Denominator cannot be 0\n Enter another number "
-        num2=gets.chomp()
-        p "#{num1} / #{num2} = #{c.division num1,num2}"
-    end
-else
-    p "Invalid option "
-end
+c.options choice,num1,num2
